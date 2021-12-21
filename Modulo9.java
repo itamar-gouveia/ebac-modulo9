4d42a1c1997181d98d9e69dc272f80edd45169c0
itamar <contato.itamargouveia@gmail.com>
2021-12-20 21:07:22 -0300

    20-12-21

---
+++ Modulo9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Modulo9{
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        int numero = scanner.nextInt();
+        
+        Integer num = numero;
+        System.out.println(num);
+        scanner.close();
+    }
+}
